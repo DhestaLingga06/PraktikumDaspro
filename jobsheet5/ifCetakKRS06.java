190aed33f4328147270a4f6aa07cb5f5e5bb2ee1
DhestaLingga06 <dhestalingga21@gmail.com>
2025-10-01 17:10:12 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS06.java
+++ jobsheet5/ifCetakKRS06.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class ifCetakKRS06 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    
+    String pesan;
+
     System.out.println("---Cetak KRS SIAKAD---");
     System.out.println("Apakah UKT sudah lunas? (true/false): ");
     boolean uktLunas = sc.nextBoolean();
@@ -10,6 +11,14 @@ public class ifCetakKRS06 {
     if (uktLunas) {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+    } else {
+        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
     }
+
+    //Struktur Ternary Operator
+   pesan = (uktLunas) ? "Pembayaran terverifikasi, silahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+    System.out.println(pesan);
+
 }
 }
