140adcbd68b384dde0bc8d487246b3b9e13f4db1
DhestaLingga06 <dhestalingga21@gmail.com>
2025-11-05 10:54:27 +0700

    Modif 1

--- JOBSHEET 9/ArrayRataNilai06.java
+++ JOBSHEET 9/ArrayRataNilai06.java
@@ -7,10 +7,15 @@ public class ArrayRataNilai06 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int jumlahLulus = 0;
 
         for(int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
+
+            if (nilaiMhs[i] > 70) {
+                jumlahLulus++;
+            }
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
@@ -18,5 +23,6 @@ public class ArrayRataNilai06 {
         }
         rata2 = total / nilaiMhs.length;
         System.out.println("Rata- rata nilai = "+rata2);
+        System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus);
     }
 }
