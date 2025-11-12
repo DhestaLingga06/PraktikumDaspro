79f3f16c0e3c41c4d0ade8aaad4ed55225697c2b
DhestaLingga06 <dhestalingga21@gmail.com>
2025-11-12 14:16:38 +0700

    Modif Dinamis

--- JOBSHEET 10/SIAKAD06.java
+++ JOBSHEET 10/SIAKAD06.java
@@ -4,7 +4,12 @@ public class SIAKAD06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] nilai = new int [4][3];
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
@@ -16,20 +21,20 @@ public class SIAKAD06 {
                 totalPersiswa += nilai[i][j];
             }
 
-            System.out.println("Nilai rata-rata: " + totalPersiswa/3);
+            System.out.println("Nilai rata-rata: " + totalPersiswa/jumlahMatkul);
 
         }
             System.out.println("\n=================================");
             System.out.println("Rata-rata Nilai setiap Mata Kuliah: ");
 
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < jumlahMatkul; j++) {
                 double totalPerMatkul = 0;
 
-                for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < jumlahSiswa; i++) {
                     totalPerMatkul += nilai[i][j];
                 }
 
-                System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul/4);
+                System.out.println("Mata Kuliah " + (j + 1) + ": " + (totalPerMatkul/jumlahSiswa));
             }
         }
     }
