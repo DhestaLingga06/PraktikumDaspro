7ec1cd46215a5deef37f283a0a7a488697c525b1
DhestaLingga06 <dhestalingga21@gmail.com>
2025-11-26 10:44:44 +0700

    Modifikasi percobaan 2

--- JOBSHEET 12/Kafe06.java
+++ JOBSHEET 12/Kafe06.java
@@ -1,6 +1,5 @@
 public class Kafe06 {
-
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
@@ -16,9 +15,16 @@ public class Kafe06 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
-    }
 
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode promo valid: Anda mendapat diskon 50% !");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo valid: Anda mendapatkan dikon 30% !");
+        } else {
+            System.out.println("Kode promo invalid.");
+        }
+    }
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Budi", true, "DISKON30");
     }
 }
